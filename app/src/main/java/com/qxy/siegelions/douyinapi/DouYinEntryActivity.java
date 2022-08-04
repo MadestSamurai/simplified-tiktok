f86f9210f07475dfff2703dd68c22783581de568
MadestSamurai <18913073128@163.com>
2022-08-04 19:37:52 +0800

    update userinfo debug run

--- app/src/main/java/com/qxy/siegelions/douyinapi/DouYinEntryActivity.java
+++ app/src/main/java/com/qxy/siegelions/douyinapi/DouYinEntryActivity.java
@@ -3,6 +3,7 @@ package com.qxy.siegelions.douyinapi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.bytedance.sdk.open.aweme.CommonConstants;
@@ -14,6 +15,18 @@ import com.bytedance.sdk.open.aweme.share.Share;
 import com.bytedance.sdk.open.douyin.DouYinOpenApiFactory;
 import com.bytedance.sdk.open.douyin.api.DouYinOpenApi;
 import com.qxy.siegelions.MainActivity;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DouYinEntryActivity extends Activity implements IApiEventHandler {
     DouYinOpenApi douYinOpenApi;
@@ -42,10 +55,78 @@ public class DouYinEntryActivity extends Activity implements IApiEventHandler {
             Intent intent = null;
             if (resp.isSuccess()) {
 
-                Toast.makeText(this, "授权成功，获得权限：" + response.grantedPermissions,
+                Toast.makeText(this, "授权成功，获得权限：" + response.grantedPermissions + response.authCode,
                         Toast.LENGTH_LONG).show();
 
+                new Thread() {
+                    @Override
+                    public void run() {
+                        networkRequest(response.authCode);
+                    }
+                }.start();
+
             }
+        }
+
+    }
+
+
+    private void networkRequest(String authCode) {
+
+        String urlTest = "https://open.douyin.com/oauth/access_token/?client_key=awobitbo8w4mf83r&" +
+                "client_secret=9108c9df11a7c5649c28e3ed426a0363&code=" + authCode + "&grant_type=authorization_code";
+        try {
+            URL url = new URL(urlTest);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = conn.getInputStream();
+            InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            StringBuffer buffer = new StringBuffer();
+            String temp = null;
+            while ((temp = bufferedReader.readLine()) != null) {
+                buffer.append(temp);
+            }
+            bufferedReader.close();
+            reader.close();
+            inputStream.close();
+            Log.e("MAIN", buffer.toString());
+            parseDiffJson(buffer.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseDiffJson(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            Log.e("Json", json);
+            String access_token = jsonObject.getJSONObject("data").getString("access_token");
+            String open_id = jsonObject.getJSONObject("data").getString("open_id");
+
+            String urlTest = "https://open.douyin.com/oauth/userinfo/?access_token="+access_token+
+                    "&open_id=" + open_id;
+            URL url = new URL(urlTest);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = conn.getInputStream();
+            InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            StringBuffer buffer = new StringBuffer();
+            String temp = null;
+            while ((temp = bufferedReader.readLine()) != null) {
+                buffer.append(temp);
+            }
+            bufferedReader.close();
+            reader.close();
+            inputStream.close();
+            Log.e("MAIN", buffer.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
@@ -55,4 +136,6 @@ public class DouYinEntryActivity extends Activity implements IApiEventHandler {
         // 错误数据
         Toast.makeText(this, "Intent出错", Toast.LENGTH_LONG).show();
     }
+
+
 }
