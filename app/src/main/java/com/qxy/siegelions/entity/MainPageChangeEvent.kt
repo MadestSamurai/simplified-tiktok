6e544e29e51beef25774d3f10bcc1f7eb6f73bdd
MadestSamurai <18913073128@163.com>
2022-08-22 08:21:59 +0800

    feature: add some entities mainly

---
+++ app/src/main/java/com/qxy/siegelions/entity/MainPageChangeEvent.kt
@@ -0,0 +1,3 @@
+package com.qxy.siegelions.entity
+
+class MainPageChangeEvent(var page: Int)
---
+++ app/src/main/java/com/qxy/siegelions/entity/ShareBean.kt
@@ -0,0 +1,8 @@
+package com.qxy.siegelions.entity
+
+/**
+ * create by libo
+ * create on 2020-06-04
+ * description
+ */
+class ShareBean(var iconRes: Int, var text: String, var bgRes: Int)
